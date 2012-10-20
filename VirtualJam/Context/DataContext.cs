06727eb13052d1c195fec3c1bda345b7c243e214
Owen Lilly <slim_shadie2@yahoo.com>
2012-10-19 22:54:35 -0500

    Added Task entity

--- VirtualJam/Context/DataContext.cs
+++ VirtualJam/Context/DataContext.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using VirtualJam.Entities;
 
 namespace VirtualJam.Context
 {
     public class DataContext : DbContext
     {
-
+        public DbSet<Task> Tasks { get; set; }
     }
 }
