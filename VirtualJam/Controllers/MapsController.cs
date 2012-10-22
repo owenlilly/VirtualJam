ff24a509b5ede7c7186f8646ef592eaefa1ae80b
Owen Lilly <slim_shadie2@yahoo.com>
2012-10-22 15:00:45 -0500

    Added maps controller and maps.js scripts

---
+++ VirtualJam/Controllers/MapsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.IO;
+
+namespace VirtualJam.Controllers
+{
+    public class MapsController : Controller
+    {
+        //
+        // GET: /Maps/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string Index(PostObj data)
+        {
+            return data.Content;
+        }
+
+        public string HttpGet(string data)
+        {
+            //return data;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://search.twitter.com/search.json?q="+data);
+            request.Method = "GET";
+            request.Timeout = 15000;
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            string result = "";
+
+            using(StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                result = reader.ReadToEnd();
+            }
+
+            return string.IsNullOrWhiteSpace(result) ? "Sorry, no data  retrieved..." : "Query string: "+data + "<br>" + result;
+        }
+    }
+
+    public class PostObj
+    {
+        public string Content { get; set; }
+    }
+}
---
+++ VirtualJam/Scripts/maps.js
@@ -0,0 +1,42 @@
+﻿//
+// Google Maps JavaScript file
+//
+$(document).ready(function () {
+
+    //
+    // The Map events
+    //
+    $('#getCenter').click(function () {
+        alert('The center is: '+ map.getCenter());
+    });
+
+    $('#getZoom').click(function () {
+        alert('The zoom is: ' + map.getZoom());
+    });
+
+    //
+    // The Map options
+    //
+    var mapDiv = document.getElementById('map'); // map element
+    var latlng = new google.maps.LatLng(18.127295215406242, -77.28318115234374); // centre coordinates (Jamaica)
+    
+    var options = {
+            center: latlng,
+            zoom: 9,
+            disableDefaultUI: true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            mapTypeControl: true,
+            mapTypeControlOptions: {
+                style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+                mapTypeIds: [
+                    google.maps.MapTypeId.ROADMAP,
+                    google.maps.MapTypeId.SATELLITE
+                ]
+            }
+            //navigationControl: true
+        }; // Options
+    
+    // The MAP
+    var map = new google.maps.Map(mapDiv, options);
+
+});
